97dffdafb3530ea0aff7616ad3ad3e8d9ed36a11
lizhenliang <zhenliang369@163.com>
2017-04-18 09:48:25 +0800

    Update salt-api-config.sh

--- salt-api-config.sh
+++ salt-api-config.sh
@@ -1,6 +1,6 @@
 yum install salt-api
-useradd -M -s /sbin/nologin saltdev
-echo "saltdev" |passwd --stdin saltdev
+useradd -M -s /sbin/nologin salt-api
+echo "salt-api" |passwd --stdin salt-api
 # salt api基于证书通信
 CRT_DIR=/etc/pki/tls/certs
 PRIVADE_KEY_DIR=/etc/pki/tls/private
@@ -18,7 +18,7 @@ rest_cherrypy:
   ssl_key: $PRIVADE_KEY_DIR/localhost_nopass.key
 external_auth:
   pam:
-    saltdev:
+    salt-api:
       - .*
       - '@wheel'
       - '@runner'
@@ -27,5 +27,5 @@ service salt-master restart
 service salt-api restart
 
 # 测试
-# curl -k https://192.168.1.99:8000/login -H "Accept: application/x-yaml" -d username='saltdev' -d password='saltdev' -d eauth='pam'
+# curl -k https://192.168.1.99:8000/login -H "Accept: application/x-yaml" -d username='salt-api' -d password='salt-api' -d eauth='pam'
 # curl -k https://192.168.1.99:8000/ -H "Accept: application/x-yaml" -H "X-Auth-Token: 8b62b1a9ae6d65b3c25774a725297e0e3316830e" -d client='local' -d tgt='*' -d fun='test.ping'
